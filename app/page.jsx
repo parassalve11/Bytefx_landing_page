793309f32aa6a31a1bdc96925973ffd6ec4bfa67
Paras Salve <parassalve112233@gmail.com>
2026-08-28 18:18:17 +0530

    Refactor code structure for improved readability and maintainability

--- app/page.jsx
+++ app/page.jsx
@@ -5,13 +5,17 @@ import { AccountTypes } from "@/components/site/AccountTypes";
 import { MobileApp } from "@/components/site/MobileApp";
 import { Funding } from "@/components/site/Funding";
 import { FinalCta } from "@/components/site/FinalCta";
+import { Hero } from "@/components/site/Hero";
+import { TradingShowcase } from "@/components/site/TradingShowcase";
 
 export default function Home() {
   return (
     <main>
+      {/* <Hero /> */}
       <Ticker />
       <Markets />
       <Conditions />
+      <TradingShowcase />
       <AccountTypes />
       <MobileApp />
       <Funding />
